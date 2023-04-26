8464ba4dbc27bdf94764973ea09aaa2b2e5ac3b9
Konstantin Lebedev <lebedev_k@tochka.com>
2023-04-26 20:15:05 +0500

    use week sheet

--- AiogramBot.py
+++ AiogramBot.py
@@ -1,13 +1,12 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, Message
 from aiogram import Bot, Dispatcher, executor, types
 from Openpyxl_func import *
-from Token import Token_API
 from datetime import date
 import calendar
 import asyncio
 import time
 
-bot = Bot(Token_API)
+bot = Bot(os.environ.get("BOT_API_TOKEN"))
 dp = Dispatcher(bot)
 lock = asyncio.Lock()
 
